ca3c3b7cf7bc94f45a5210026e36af7ace1c47c5
Ankush  Baburao Yamkar <yamkar.ab10@gmail.com>
2024-02-06 11:12:53 +0530

    removed the production response

--- backend/index.js
+++ backend/index.js
@@ -19,16 +19,6 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
-    )
-  );
-}
-
 var smtpTransport = nodemailer.createTransport({
   host: "smtpout.secureserver.net",
   port: 465,
